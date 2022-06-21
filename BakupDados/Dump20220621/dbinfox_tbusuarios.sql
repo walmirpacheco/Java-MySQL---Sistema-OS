97b5969dbfb7e6d2da601dd4b8af5a059fcb62a4
Walmir Pacheco <walmir.pacheco34@gmail.com>
2022-06-21 16:19:17 -0300

    Comecei a criar tela de cadastro de clientes

---
+++ BakupDados/Dump20220621/dbinfox_tbusuarios.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dbinfox
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tbusuarios`
+--
+
+DROP TABLE IF EXISTS `tbusuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tbusuarios` (
+  `iduser` int NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `fone` varchar(15) DEFAULT NULL,
+  `login` varchar(15) NOT NULL,
+  `senha` varchar(15) NOT NULL,
+  `perfil` varchar(20) NOT NULL,
+  PRIMARY KEY (`iduser`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tbusuarios`
+--
+
+LOCK TABLES `tbusuarios` WRITE;
+/*!40000 ALTER TABLE `tbusuarios` DISABLE KEYS */;
+INSERT INTO `tbusuarios` VALUES (1,'Walmir Pacheco','419999-9999','walmirpacheco','123456','admin'),(2,'Administrador','412222-2222','admin','admin','admin'),(3,'Leandro Ramos','415555-5555','leandro','123','user'),(4,'Bill Gates','414444-4444','bill','123','user'),(5,'Jefferson Costa','415555-5555','jefferson','123456','user'),(6,'Linus Torvalds','419999-9999','linus','123','admin'),(7,'Robson Vaamonde','417777-7777','robson','123','admin'),(8,'Bruce Dickison','416666-6666','iron','maiden','admin'),(9,'Angelina Jolie','4199999999','lara','croft','user');
+/*!40000 ALTER TABLE `tbusuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-06-21 13:52:37
